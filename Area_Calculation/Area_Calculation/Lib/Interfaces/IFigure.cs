2949b6e3d6819de3187ead9f25fc7e5e72e5246a
Emiliya Melnikova <binarytoucan@gmail.com>
2023-05-26 17:46:43 +0300

    Add Interface

---
+++ Area_Calculation/Area_Calculation/Lib/Interfaces/IFigure.cs
@@ -0,0 +1,15 @@
+﻿namespace Area_Calculation.Lib.Interfaces
+{
+    /// <summary>
+    /// Интерфес по созданию фигур
+    /// </summary>
+    public interface IFigure
+    {
+        /// <summary>
+        /// Метод по расчету площади
+        /// </summary>
+        /// <returns> Площадь фигуры </returns>
+        public double Area();
+
+    }
+}
